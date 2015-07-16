367244f8ce233c022afad0186b03fbfd218a4692
dgarmar <dgarmar@users.noreply.github.com>
2015-07-16 19:57:48 +0200

    Create prepare.sharing.sh

---
+++ 4.Blueprint/Sharing/prepare.sharing.sh
@@ -0,0 +1,25 @@
+
+#!/bin/bash
+
+# Prepare files for sQTL sharing estimation
+
+# Define environmental variables
+
+export WD='/users/rg/dgarrido/run-blueprint/run/results'
+export PROTCOD='/users/rg/dgarrido/enrichments/blueprint/files/protcod.list'
+
+cd $WD
+
+# Loop over tissues and get pairs sQTL-sGene
+
+for i in $( ls -d */ ); do
+	echo $i
+	cd $WD/$i/flux # Also flux
+	mkdir sharing
+	cd sharing	
+
+	grep -F -w -f $PROTCOD ../sQTLs-sig5.tsv | cut -f1-2 | sort | uniq > pairs-sig5.list
+	grep -F -w -f $PROTCOD ../sQTLs-sig1.tsv | cut -f1-2 | sort | uniq > pairs-sig1.list
+	grep -F -w -f $PROTCOD ../sQTLs-all.tsv | cut -f1-2,10 | sort | uniq > pairs-all.list
+	
+done
