f3fb7642b832c982aa92da3550b85f1f5bf243b8
dgarmar <dgarmar@users.noreply.github.com>
2015-07-16 13:56:20 +0200

    Create sqtls.sh

---
+++ 3.GTEx/per-tissue/sqtls.sh
@@ -0,0 +1,15 @@
+
+## sqtls.R submission script
+
+#!/bin/bash
+
+#$ -N sqtls  
+#$ -cwd
+#$ -M diego.garrido@crg.eu -m abe 
+#$ -q short-sl65 
+#$ -o ../../clust_out/$JOB_NAME-$JOB_ID.out 
+#$ -e ../../clust_err/$JOB_NAME-$JOB_ID.err
+#$ -l virtual_free=4G 
+#$ -l h_rt=01:00:00
+
+Rscript ../../bin/sqtls.R 2>>stderr.txt >>stdout.txt
