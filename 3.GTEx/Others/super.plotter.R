aa67958536d57975df9a2b23c29e5bfb46b75a53
dgarmar <dgarmar@users.noreply.github.com>
2015-07-16 18:10:01 +0200

    Create super.plotter.R

---
+++ 3.GTEx/Others/super.plotter.R
@@ -0,0 +1,15 @@
+
+#!/bin/bash
+
+#Iterate over all tissues and plot results (sQTls-sGenes)
+
+export WD='/users/rg/dgarrido/run-GTEx1/results'
+
+cd $WD
+
+# Loop over tissues
+
+for i in $( ls -d * ); do
+	echo $i
+	Rscript /users/rg/dgarrido/autoplot/autoplot.R $i
+done	
