aa0946abb66463124af52f9346bbce2256dd5fa0
dgarmar <dgarmar@users.noreply.github.com>
2015-07-16 18:39:10 +0200

    Create index_geno.R

---
+++ 4.Blueprint/sQTLseekeR-run/index_geno.R
@@ -0,0 +1,22 @@
+##
+## Index genotype file (if not done externally before)
+##
+
+## WD
+setwd("/nfs/users/rg/dgarrido/run-blueprint/run/bin")
+
+## Look at the paths in the specified order when loading a library
+.libPaths(c("/nfs/users/rg/dgarrido/R/x86_64-redhat-linux-gnu-library/3.1",
+            "/nfs/software/R/packages"))
+
+## Command line input
+args <- commandArgs(TRUE)
+
+library(sQTLseekeR)
+
+## Input file: genotype (required format)
+genotype.f = args[1]
+
+## Index the genotype file 
+index.genotype(genotype.f)
+
