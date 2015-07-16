2a90273b2c75fe625da7750ffa346461005aad5f
dgarmar <dgarmar@users.noreply.github.com>
2015-07-16 18:48:33 +0200

    Update multi_sqtl.seeker.R

--- 4.Blueprint/sQTLseekeR-run/multi_sqtl.seeker.R
+++ 4.Blueprint/sQTLseekeR-run/multi_sqtl.seeker.R
@@ -1,16 +1,17 @@
 
 ##
-##  Iterate through cell type folders to run sqtlseeker.R in a per celltype/quantifier manner
+##  Iterate through cell type folders to run prepare.trans.exp.R in a 
+##  per cell type and per quantifier(Flux,Cufflinks) manner 
 ##
 
 ## WD
 setwd("/nfs/users/rg/dgarrido/run-blueprint/run/")
 
 ## Getting cell type (folder) names 
-sample.tissues <- read.table(file="input/samples-celltypes.txt", header=TRUE,sep="\t", as.is=TRUE)
-celltypes <- sort(unique(sample.tissues[,2]))
+sample.celltypes <- read.table(file="input/samples-celltypes.txt", header=TRUE,sep="\t", as.is=TRUE)
+celltypes <- sort(unique(sample.celltypes[,2]))
 
-## Iterate and run sqtlseeker.R for each celltype
+## Iterate and run sqtlseeker.R for each celltype and quantifier
 for (celltype in celltypes){
   setwd(sprintf("results/%s/flux",celltype))
   system("Rscript ../../../bin/sqtlseeker.R 2>> stderr.txt >> stdout.txt")
