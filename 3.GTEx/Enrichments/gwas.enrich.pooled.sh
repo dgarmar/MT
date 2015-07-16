660a00406f393c34c99fc77613e872248f548911
dgarmar <dgarmar@users.noreply.github.com>
2015-07-16 14:48:42 +0200

    Update gwas.enrich.pooled.sh

--- 3.GTEx/Enrichments/gwas.enrich.pooled.sh
+++ 3.GTEx/Enrichments/gwas.enrich.pooled.sh
@@ -19,7 +19,7 @@ SQTLS_PERC_IN=$(echo "100*$SQTLS_IN/$SQTLS_NB" | bc -l)
 NON_SQTLS_PERC_IN=$(echo "100*$NON_SQTLS_IN/$NON_SQTLS_NB" | bc -l)
 
 RATIO5=$(echo "$SQTLS_PERC_IN/$NON_SQTLS_PERC_IN" | bc -l)
-echo $RATIO5
+echo $RATIO5 # Enrichment in exons of sQTLs at 5% FDR
 
 SQTLS_IN="$(bedtools intersect -a pooled.sqtls.1fdr.bed -b $GWAS_BED -c | awk '$5>0 {print $4"\t"$5}' | wc -l)"
 NON_SQTLS_IN="$(bedtools intersect -a pooled.non-sqtls.1fdr.bed -b $GWAS_BED -c | awk '$5>0 {print $4"\t"$5}' | wc -l)"
@@ -31,5 +31,5 @@ SQTLS_PERC_IN=$(echo "100*$SQTLS_IN/$SQTLS_NB" | bc -l)
 NON_SQTLS_PERC_IN=$(echo "100*$NON_SQTLS_IN/$NON_SQTLS_NB" | bc -l)
 
 RATIO1=$(echo "$SQTLS_PERC_IN/$NON_SQTLS_PERC_IN" | bc -l)
-echo $RATIO1
+echo $RATIO1 # Enrichment in exons of sQTLs at 1% FDR
 
