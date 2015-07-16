e88c2b28a0585025817b30383b8e8d014697b54f
dgarmar <dgarmar@users.noreply.github.com>
2015-07-16 14:38:29 +0200

    Create sqtls.sh

---
+++ 3.GTEx/per-tissue-and-gender/sqtls.sh
@@ -0,0 +1,13 @@
+# Submission script for sqtls.R
+
+#!/bin/bash
+#$ -N sqtls  
+#$ -cwd
+##$ -M diego.garrido@crg.eu -m abe 
+#$ -q short-sl65 
+#$ -o ../../../clust_out/$JOB_NAME-$JOB_ID.out 
+#$ -e ../../../clust_err/$JOB_NAME-$JOB_ID.err
+#$ -l virtual_free=20G 
+#$ -l h_rt=01:00:00
+
+Rscript ../../../bin/sqtls.R 2>>stderr.txt >>stdout.txt
