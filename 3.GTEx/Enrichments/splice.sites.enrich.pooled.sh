69898ee7241543fcad4fd6a979cdc066a81ef4f0
dgarmar <dgarmar@users.noreply.github.com>
2015-07-16 14:54:11 +0200

    Update splice.sites.enrich.pooled.sh

--- 3.GTEx/Enrichments/splice.sites.enrich.pooled.sh
+++ 3.GTEx/Enrichments/splice.sites.enrich.pooled.sh
@@ -1,29 +1,34 @@
 #!/bin/bash
 
-# pooled sQTLs - enrichment in splice sites
+# pooled sQTLs - enrichments in splice sites
 
 # Define all environment variables
 
 export WD='/users/rg/dgarrido/run-GTEx1/results'
 export SSITES='/users/rg/dgarrido/enrichments/gtex/all/splice_sites/splice.sites.bed'
 
-cd $WD
+cd $WD/globalenrich
 
-# Loop over tissues
+SQTLS_IN="$(bedtools intersect -a pooled.sqtls.bed -b $SSITES -c | awk '$5>0 {print $4"\t"$5}' | wc -l)"
+NON_SQTLS_IN="$(bedtools intersect -a pooled.non-sqtls.bed -b $SSITES -c | awk '$5>0 {print $4"\t"$5}' | wc -l)"
 
-for i in $( ls -d * ); do
-	echo $i
+SQTLS_NB="$(cat pooled.sqtls.bed | wc -l)"
+NON_SQTLS_NB="$(cat pooled.non-sqtls.bed | wc -l)"
 
-	SQTLS_IN="$(bedtools intersect -a $i/enrichments/exons/sqtls.bed -b $SSITES -c | awk '$5>0 {print $4"\t"$5}' | wc -l)"
-	NON_SQTLS_IN="$(bedtools intersect -a $i/enrichments/exons/non-sqtls.bed -b $SSITES -c | awk '$5>0 {print $4"\t"$5}' | wc -l)"
+SQTLS_PERC_IN=$(echo "100*$SQTLS_IN/$SQTLS_NB" | bc -l)
+NON_SQTLS_PERC_IN=$(echo "100*$NON_SQTLS_IN/$NON_SQTLS_NB" | bc -l)
 
-	SQTLS_NB="$(cat $i/enrichments/exons/sqtls.bed | wc -l)"
-	NON_SQTLS_NB="$(cat $i/enrichments/exons/non-sqtls.bed | wc -l)"
+RATIO5=$(echo "$SQTLS_PERC_IN/$NON_SQTLS_PERC_IN" | bc -l)
+echo $RATIO5 # Fold enrichment in splice sites at 5% FDR
 
-	SQTLS_PERC_IN=$(echo "100*$SQTLS_IN/$SQTLS_NB" | bc -l)
-	NON_SQTLS_PERC_IN=$(echo "100*$NON_SQTLS_IN/$NON_SQTLS_NB" | bc -l)
+SQTLS_IN="$(bedtools intersect -a pooled.sqtls.1fdr.bed -b $SSITES -c | awk '$5>0 {print $4"\t"$5}' | wc -l)"
+NON_SQTLS_IN="$(bedtools intersect -a pooled.non-sqtls.1fdr.bed -b $SSITES -c | awk '$5>0 {print $4"\t"$5}' | wc -l)"
 
-	RATIO=$(echo "$SQTLS_PERC_IN/$NON_SQTLS_PERC_IN" | bc -l)	
-	echo $RATIO
+SQTLS_NB="$(cat pooled.sqtls.1fdr.bed | wc -l)"
+NON_SQTLS_NB="$(cat pooled.non-sqtls.1fdr.bed | wc -l)"
 
-done
+SQTLS_PERC_IN=$(echo "100*$SQTLS_IN/$SQTLS_NB" | bc -l)
+NON_SQTLS_PERC_IN=$(echo "100*$NON_SQTLS_IN/$NON_SQTLS_NB" | bc -l)
+
+RATIO1=$(echo "$SQTLS_PERC_IN/$NON_SQTLS_PERC_IN" | bc -l)
+echo $RATIO1 # Fold enrichment in splice sites at 1% FDR
